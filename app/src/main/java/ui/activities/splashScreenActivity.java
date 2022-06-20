d111901bcb69532a266ad2d6b0a3cd2804c63728
priyanshu <priyanshukumar025@gmail.com>
2022-06-21 00:44:39 +0530

    version 1

---
+++ app/src/main/java/ui/activities/splashScreenActivity.java
@@ -0,0 +1,31 @@
+package ui.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.iotashopping.R;
+import com.google.firebase.FirebaseApp;
+
+public class splashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            FirebaseApp.initializeApp(splashScreenActivity.this);
+            Intent intent=new Intent(splashScreenActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    },2500);
+
+    }
+}
